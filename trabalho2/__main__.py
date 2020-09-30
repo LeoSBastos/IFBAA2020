0bfa7549c6022dbddd261d3b18d0d3c69f8b22a9
Leonardo Bastos <leosbastos7@gmail.com>
2020-09-30 09:57:12 -0300

    Finalizando o algoritmo e pegando os dados do Trabalho 2

--- trabalho2/__main__.py
+++ trabalho2/__main__.py
@@ -1,12 +1,35 @@
+import argparse
+import time
+
 from read import *
 from knapsack import Knapsack
+
 if __name__ == "__main__":
-    file = Read("entradas/entrada1.csv")
+    parser = argparse.ArgumentParser(
+        description="Trabalho 2 de Análise de Algoritmo - Problema da Mochila")
+    parser.add_argument(
+        "inputFilename", metavar="INPUT_FILENAME", type=str, nargs="+", help="O nome do arquivo de entrada, e.g. entrada1.csv")
+    parser.add_argument(
+        "outputFilename", metavar="OUTPUT_FILENAME", type=str, nargs="+", help="O nome do arquivo de saída, e.g. saida1.txt"
+    )
+    args = parser.parse_args()
+    file = Read(args.inputFilename[0])
     data = file.read()
-    for chunk in data:
-        knap = Knapsack(chunk)
-        knap.allLights()
-        knap.costBenefit()
-        print(knap.lightIndexes)
-        print(knap.costBenefitIndexes)
-        break
+    with open(args.outputFilename[0], "w") as file:
+        for i, chunk in enumerate(data):
+            print(f'Trabalhando no caso {i+1}')
+            file.write(f"Teste de caso número {i+1}\n")
+            knap = Knapsack(chunk)
+            start_time = time.time()
+            knap.allLights()
+            file.write(
+                f'Valor dos menores pesos e seu tempo: {knap.lightValue} ({time.time() - start_time})\n')
+            start_time = time.time()
+            knap.costBenefit()
+            file.write(
+                f'Valor dos melhores custo-benefícios e seu tempo: {knap.costBenefitValue} ({time.time() - start_time})\n')
+            start_time = time.time()
+            knap.dynamic()
+            file.write(
+                f'Valor do algoritmo dinâmico e seu tempo: {knap.optimizedValue} ({time.time() - start_time})\n')
+            file.write("----------\n")
--- trabalho2/knapsack.py
+++ trabalho2/knapsack.py
@@ -5,35 +5,52 @@ class Knapsack:
     def __init__(self, chunk):
         self.chunk = chunk
         self.knapsackSize = 10000
+        self.objectSize = 1000
         self.lightQueue = PriorityQueue()
         self.costBenefitQueue = PriorityQueue()
-        self.optimizedIndexes = []
-        self.lightIndexes = []
-        self.costBenefitIndexes = []
-        self.optimizedWeight = 0
-        for r in self.chunk.itertuples():
-            self.lightQueue.put((r[3], r[1]))
-            self.costBenefitQueue.put((-1*(r[2]/r[3]), (r[3], r[1])))
+        self.lightValue = 0
+        self.costBenefitValue = 0
+        self.optimizedValue = 0
+        self.weights = []
+        self.values = []
 
     def allLights(self):
+        for r in self.chunk.itertuples():
+            self.lightQueue.put((r[3], r[2]))
         knapsackSizeLeft = self.knapsackSize
-        while knapsackSizeLeft > 0:
-            weigth, index = self.lightQueue.get()
+        while not self.lightQueue.empty():
+            weigth, value = self.lightQueue.get()
             if (knapsackSizeLeft - weigth) < 0:
-                break
-            self.lightIndexes.append(index)
+                continue
+            self.lightValue += value
             knapsackSizeLeft -= weigth
 
     def costBenefit(self):
+        for r in self.chunk.itertuples():
+            self.costBenefitQueue.put((-1*(r[2]/r[3]), (r[3], r[2])))
         knapsackSizeLeft = self.knapsackSize
         while not self.costBenefitQueue.empty():
-            if knapsackSizeLeft == 0:
-                break
-            value, (weigth, index) = self.costBenefitQueue.get()
-            if (knapsackSizeLeft - weigth) < 0:
+            costBenefitValue, (weigth, value) = self.costBenefitQueue.get()
+            if (knapsackSizeLeft - weigth) <= 0:
                 continue
-            self.costBenefitIndexes.append(index)
+            self.costBenefitValue += value
             knapsackSizeLeft -= weigth
 
     def dynamic(self):
-        return
+        for r in self.chunk.itertuples():
+            self.weights.append(r[3])
+            self.values.append(r[2])
+        matrix = [[0 for x in range(self.knapsackSize+1)]
+                  for m in range(self.objectSize+1)]
+        for i in range(1, self.objectSize+1):
+            for j in range(1, self.knapsackSize+1):
+                if(self.weights[i-1] <= j):
+                    if(self.values[i-1] +
+                       matrix[i-1][j-self.weights[i-1]] > matrix[i - 1][j]):
+                        matrix[i][j] = self.values[i - 1] + \
+                            matrix[i - 1][j - self.weights[i - 1]]
+                    else:
+                        matrix[i][j] = matrix[i - 1][j]
+                else:
+                    matrix[i][j] = matrix[i-1][j]
+        self.optimizedValue = matrix[self.objectSize][self.knapsackSize]
